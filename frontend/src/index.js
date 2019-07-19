fd68b02f368cb08b53e5ed445c99f26c21e64c07
Rock Kim <purasle@gmail.com>
2019-07-19 08:20:46 -0400

    basic routing
    basic routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import EmployeeContainer from './container/EmployeeContainer'
 import * as serviceWorker from './serviceWorker';
+import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(<EmployeeContainer />, document.getElementById('root'));
+const router = (
+  <Router>
+    <div>
+      <Route exact path="/" component={ EmployeeContainer } />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(
+  router,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
